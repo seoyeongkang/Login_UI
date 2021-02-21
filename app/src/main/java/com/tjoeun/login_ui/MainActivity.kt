48d6c8a9118ba5f37c2f293abe0c1a12662ce53e
seoyeongkang <jumprope123@naver.com>
2021-02-21 12:10:14 +0900

    버튼 클릭시 > 입력값 받아와서 if (AND) 조건 검사

--- app/src/main/java/com/tjoeun/login_ui/MainActivity.kt
+++ app/src/main/java/com/tjoeun/login_ui/MainActivity.kt
@@ -2,11 +2,28 @@ package com.tjoeun.login_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loginBtn.setOnClickListener {
+
+//            입력된 이메일 / 비번 가져오기
+            val inputEmail = editEm.text.toString()
+            val inputPw = editPw.text.toString()
+
+//            가져온 이메일/비번 값 검사
+
+            if(inputEmail == "admin@test.com" && inputPw == "qwer"){
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인에 실패했습니다..", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
